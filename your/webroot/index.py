218943175ef599081d96441c58fc5d450b5ca6d7
Nicolas Gomez <ng1968@nyu.edu>
2019-10-12 20:48:33 -0400

    Added SQL database and cookies support. Also CSRF protection

--- your/webroot/index.py
+++ your/webroot/index.py
@@ -1,119 +1,124 @@
-from flask import Flask, escape, redirect, render_template, request, session, url_for
+from flask import Flask, make_response, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies, get_raw_jwt
+)
 import subprocess
+import time
 
+app = Flask(__name__)
 
-def create_app(config=None):
-  DATABASE = 'database.txt'
-  app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
 
-  app.secret_key = b'd338cdce585dc7662749c2282b5b1d0938fd7c102a9ba14ad0bab6057a7cf9da5d53d9b690a5a43503e2f66c56894b6616ba2c4398e47e8b82cc6c10ee8ce372'
+db = SQLAlchemy(app)
+@app.before_first_request
+def create_tables():
+  db.create_all()
 
-  @app.route('/')
-  def index():
-    # The root directory automatically redirects to login.
-    return redirect(url_for('login'))
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/spell_check'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_CSRF_CHECK_FORM'] = True
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
 
-  @app.route('/login', methods=['POST', 'GET'])
-  def login():
-    error = None
-    if request.method == 'POST':
-      login_valid = validate_login(request.form['uname'], request.form['pword'], request.form['2fa'])
-      if login_valid == 1:
-        session['username'] = request.form['uname']
-        return  render_template('login.html', login_output='success')
-      elif login_valid == 2:
-        return  render_template('login.html', login_output='incorrect')
-      elif login_valid == 3:
-        return  render_template('login.html', login_output='failure: two-factor wrong')
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+import models
 
-  def validate_login( uname, pword, twofa):
-    # Checks if credential match.
-    # 1 = Valid login.
-    # 2 = incorrect username or password.
-    # 3 = two-fa wrong.
-    login_found = 0
-    # Opens file
-    with open(DATABASE, 'r') as read_descriptor:
-      line_input = read_descriptor.readline()
+@app.route('/')
+def index():
+  # The root directory automatically redirects to login.
+  return redirect(url_for('login'))
 
-      # Loop that reads file line by line.
-      while line_input:
-        line = line_input.split()
-        # If they left the two-fa field empty
-        if twofa == '':
-          if( line[0] == uname and line[1] == pword and line[2] == 'empty' ):
-            login_found = 1
-            break
-          elif( line[0] == uname and line[1] == pword and line[2] != 'empty' ):
-            login_found = 3
-            break
-          elif( line[0] != uname or line[1] != pword ):
-            login_found = 2
-        else:
-          if( line[0] == uname and line[1] == pword and line[2] == twofa ):
-            login_found = 1
-            break
-          elif( line[0] == uname and line[1] == pword and line[2] != twofa ):
-            login_found = 3
-            break
-          elif( line[0] != uname or line[1] != pword ):
-            login_found = 2
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+  if request.method == 'POST':
+    current_user = models.UserModel.find_by_username(request.form['uname'])
 
-        line_input = read_descriptor.readline()
+    if not current_user:
+      return make_response(render_template('login.html', login_output='incorrect')), 401
     
-    return login_found
+    if models.UserModel.verify_hash(request.form['pword'], current_user.password):
+      if models.UserModel.verify_hash(request.form['2fa'], current_user.two_factor):
+        access_token = create_access_token(identity = request.form['uname'])
+        refresh_token = create_refresh_token(identity = request.form['uname'])
+        resp = make_response(render_template('login.html', login_output='success'))
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
+        return  resp, 200
+      else:
+        return  make_response(render_template('login.html', login_output='failure: two-factor wrong')), 401
+    else:
+      return  make_response(render_template('login.html', login_output='incorrect')), 401
 
-  @app.route('/register', methods=['POST', 'GET'])
-  def register():
-    error = None
-    if request.method == 'POST':
-      # Checks if the username is already in the database.
-      with open(DATABASE, 'r') as read_descriptor:
-        if request.form['uname'] in read_descriptor.read():
-          return render_template('register.html', register_output='Failure! Username already exists, try again')
-        else:
-          # Writes the registration information to the database.
-          with open(DATABASE, 'a+') as write_descriptor:
-            # If the 2FA if not entered we give it a default value.
-            if request.form['2fa'] == '':
-              registration_info = '{} {} {}\n'.format(request.form['uname'], request.form['pword'], 'empty')
-            else:
-              registration_info = '{} {} {}\n'.format(request.form['uname'], request.form['pword'], request.form['2fa'])
-            write_descriptor.write(registration_info)
-            return render_template('register.html', register_output='Account registered Success!')
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('register.html', error=error)
+  # was GET
+  return make_response(render_template('login.html')), 200
 
-  @app.route('/spell_check', methods=['POST', 'GET'])
-  def spell_check():
-    error = None
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+  if request.method == 'POST':
+    # Checks if the username is already in the database.
+    if models.UserModel.find_by_username(request.form['uname']):
+      return make_response(render_template('register.html', register_output='Failure! Username already exists, try again'))
+    
+    new_user = models.UserModel(
+      username = request.form['uname'],
+      password = models.UserModel.generate_hash(request.form['pword']),
+      two_factor = models.UserModel.generate_hash(request.form['2fa'])
+    )
+    
+    try:
+      new_user.save_to_db()
+      access_token = create_access_token(identity = request.form['uname'])
+      refresh_token = create_refresh_token(identity = request.form['uname'])
+      return make_response(render_template('register.html', register_output='Account registered Success!')), 200
+    except:
+      return make_response(render_template('register.html', register_output='Something went wrong!')), 500
 
-    if request.method == 'POST':
-      filename = 'input_file_%s.txt' % escape(session['username'])
-      with open(filename, 'w+') as write_descriptor:
-        write_descriptor.write(request.form['inputtext'])
+  # was GET
+  return make_response(render_template('register.html')), 200
 
-      command = ['./spell_check', filename, 'wordlist.txt']
-      output = subprocess.run(command, stdout=subprocess.PIPE)
-      return render_template('spell_check.html', textout=request.form['inputtext'], misspelled=output.stdout.decode('utf-8'))
+@app.route('/spell_check', methods=['POST', 'GET'])
+@jwt_required
+def spell_check():
+  if request.method == 'POST':
+    filename = 'input_file_%s.txt' % str(time.time())
+    with open(filename, 'w+') as write_descriptor:
+      write_descriptor.write(request.form['inputtext'])
 
-    return render_template('spell_check.html', error=error)
+    command = ['./spell_check', filename, 'wordlist.txt']
+    output = subprocess.run(command, stdout=subprocess.PIPE)
+    subprocess.run(['rm', filename], stdout=subprocess.PIPE)
+    current_user = get_jwt_identity()
+    return make_response(render_template('spell_check.html', textout=request.form['inputtext'], csrf_token=(get_raw_jwt() or {}).get("csrf"), misspelled=output.stdout.decode('utf-8'))), 200
+
+  return make_response(render_template('spell_check.html', csrf_token=(get_raw_jwt() or {}).get("csrf"))), 200
 
 
-  @app.route('/logout')
-  def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('login'))
+@app.route('/token/refresh', methods=['POST', 'GET'])
+@jwt_refresh_token_required
+def token_refresh():
+  current_user = get_jwt_identity()
+  access_token = create_access_token(identity = current_user)
+  resp = make_response(render_template('login.html', login_output='success'))
+  set_refresh_cookies(resp, refresh_token)
+  return resp, 200
 
-  @app.errorhandler(404)
-  def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+@app.route('/logout')
+def logout():
+  # remove the username from the session if it's there
+  try:
+    resp = make_response(render_template('login.html', login_output='success'))
+    unset_jwt_cookies(resp)
+    return redirect(url_for('login')), 200
+  except:
+    return make_response(render_template(error='Something went wrong')), 500
 
-  return app
-
-app = create_app()
+@app.errorhandler(404)
+def page_not_found(error):
+  return make_response(render_template('page_not_found.html')), 404
