74dde2c9ff04609206039def65bf9e019803d584
Nicolas Gomez <ng1968@nyu.edu>
2019-10-15 23:31:45 -0400

    Added test and tox call on travis

--- your/webroot/test_flask.py
+++ your/webroot/test_flask.py
@@ -13,8 +13,10 @@ Documentation:
 import pytest
 from app import app
 import models
+import re
 
-from flask import Flask
+from flask import Flask, make_response, render_template
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_raw_jwt
 
 @pytest.fixture
 def client():
@@ -62,9 +64,72 @@ def test_register_entry_already_exists(client):
   assert b"<p id=\"success\">Failure! Username already exists, try again<p>" in res.data
 
 
+def test_login_valid(client):
+  # Tests if register page loads properly.
+  sent = {"uname" : "test",
+          "pword" : "test",
+          "2fa" : "test"}
+  res = client.post("/login", data=sent)
+  assert res.status_code == 200
+  assert b"<p id=\"result\">success</p>" in res.data
+
+
+def test_login_invalid(client):
+  # Tests if register page loads properly.
+  sent = {"uname" : "test",
+          "pword" : "test1",
+          "2fa" : "test"}
+  res = client.post("/login", data=sent)
+  assert res.status_code == 401
+  assert b"<p id=\"result\">incorrect</p>" in res.data
+
+
+def test_login_invalid_2fa(client):
+  # Tests if register page loads properly.
+  sent = {"uname" : "test",
+          "pword" : "test",
+          "2fa" : "test1"}
+  res = client.post("/login", data=sent)
+  assert res.status_code == 401
+  assert b"<p id=\"result\">failure: two-factor wrong</p>" in res.data
+
+
 def test_spell_check_page_no_access(client):
   # Checks if a 401 error code is received.
   # This should be the case since they do not have a cookie yet.
   res = client.get("/spell_check")
   assert res.status_code == 401
   assert b"Missing cookie" in res.data
+
+
+def test_spell_check_page_access(client):
+  # Checks if a 401 error code is received.
+  # This should be the case since they do not have a cookie yet.
+  sent = {"uname" : "test",
+          "pword" : "test",
+          "2fa" : "test"}
+  res = client.post("/login", data=sent)
+  res = client.get("/spell_check")
+  assert res.status_code == 200
+  assert b"Spell Check - Flaskr" in res.data
+
+
+def test_spell_check_page_access_input(client):
+  # Checks if a 401 error code is received.
+  # This should be the case since they do not have a cookie yet.
+  sent = {"uname" : "test",
+          "pword" : "test",
+          "2fa" : "test"}
+  res = client.post("/login", data=sent)
+
+  res = client.get("/spell_check")
+  cookie = re.findall(r"(([a-z0-9]+)-([a-zA-Z0-9]+)-([a-zA-Z0-9]+)-([a-zA-Z0-9]+)-([a-z0-9]+))\w", res.data.decode("utf-8"))[0][0]
+  sent_to_post = {"inputtext" : "test",
+                  "csrf_token" : cookie}
+
+  res1 = client.post("/spell_check", data=sent_to_post)
+  print(res1.data)
+  
+  """res = client.post("/spell_check", data=sent_to_post)
+  assert res.status_code == 200
+  #assert b"<p id=\"textout\">test</p>" in res.data"""
