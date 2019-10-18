5b2a11ca129a494a49e1b6a1245aa4171d54b2a5
ng1968 <54967016+ng1968@users.noreply.github.com>
2019-10-17 18:29:49 -0700

    Update test_flask.py

--- your/webroot/test_flask.py
+++ your/webroot/test_flask.py
@@ -1,15 +1,3 @@
-"""
-You can auto-discover and run all tests with this command:
-
-    $ pytest
-
-Documentation:
-
-* https://docs.pytest.org/en/latest/
-* https://docs.pytest.org/en/latest/fixture.html
-* http://flask.pocoo.org/docs/latest/testing/
-"""
-
 import pytest
 from app import app
 import models
